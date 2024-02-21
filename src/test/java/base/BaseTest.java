7c580404826515deb04c586155f06c8269521f97
Abhinav <abhinavsingh@o2h.com>
2024-02-21 14:23:22 +0530

    added-launch-methods-specific-to-browsers

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,20 +2,37 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
     protected WebDriver driver;
-
     @BeforeMethod
     public void startSession() {
         setUp();
         driver.get("https://the-internet.herokuapp.com/");
     }
 
-    private WebDriver setUp() {
+    private void setUp() {
+        launchChromeDriver();
+    }
+    private WebDriver launchChromeDriver(){
         driver = new ChromeDriver();
+        return driver;
+    }
+    private WebDriver launchFirefoxDriver(){
+        driver = new FirefoxDriver();
+        return driver;
+    }
+    private WebDriver launchSafariDriver(){
+        driver=new SafariDriver();
+        return driver;
+    }
+    private WebDriver launchEdgeDriver(){
+        driver=new EdgeDriver();
         return driver;
     }
 
