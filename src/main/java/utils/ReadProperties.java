0d248a45326da843e1a49da8cde1a6c699398120
Abhinav <abhinavsingh@o2h.com>
2024-02-22 18:15:46 +0530

    added-logic-for-fetching-value-from-properties-file-based-on-the-fact-if-key-is-present-in-properties-file

--- src/main/java/utils/ReadProperties.java
+++ src/main/java/utils/ReadProperties.java
@@ -12,11 +12,12 @@ import java.util.LinkedHashMap;
 public class ReadProperties {
     private static HashMap<String, String> globalProperties = new LinkedHashMap<String, String>();
     private static HashMap<String, String> browserProperties = new LinkedHashMap<String, String>();
+    private static final String PATH_TO_PROPERTIES_DIRECTORY = "/src/main/resources/properties_files/";
 
     private static HashMap<String, String> createGlobalPropertiesConfiguration() {
         Configurations globalConfigurations = new Configurations();
         try {
-            Configuration config = globalConfigurations.properties(new File(System.getProperty("user.dir") + "/src/main/resources/properties_files/global.properties"));
+            Configuration config = globalConfigurations.properties(new File(System.getProperty("user.dir") + PATH_TO_PROPERTIES_DIRECTORY + "global.properties"));
             Iterator<String> globalKeys = config.getKeys();
             while (globalKeys.hasNext()) {
                 String key = globalKeys.next();
@@ -34,7 +35,7 @@ public class ReadProperties {
     private static HashMap<String, String> createBrowserPropertiesConfiguration() {
         Configurations browserConfigurations = new Configurations();
         try {
-            Configuration config = browserConfigurations.properties(new File(System.getProperty("user.dir") + "/src/main/resources/properties_files/browser.properties"));
+            Configuration config = browserConfigurations.properties(new File(System.getProperty("user.dir") + PATH_TO_PROPERTIES_DIRECTORY + "browser.properties"));
             Iterator<String> browserKeys = config.getKeys();
             while (browserKeys.hasNext()) {
                 String key = browserKeys.next();
@@ -46,6 +47,32 @@ public class ReadProperties {
             System.out.println(cex.getMessage());
             browserProperties.clear();
             return browserProperties;
+        }
+    }
+
+    private static void createGlobalPropertiesWhenMapIsEmpty() {
+        if (globalProperties.isEmpty()) {
+            createGlobalPropertiesConfiguration();
+        } else {
+            return;
+        }
+    }
+
+    private static void createBrowserPropertiesWhenMapIsEmpty() {
+        if (browserProperties.isEmpty()) {
+            createBrowserPropertiesConfiguration();
+        } else {
+            return;
+        }
+    }
+
+    public static String getValue(String key) {
+        createGlobalPropertiesWhenMapIsEmpty();
+        createBrowserPropertiesConfiguration();
+        if (globalProperties.containsKey(key)) {
+            return globalProperties.get(key);
+        } else {
+            return browserProperties.get(key);
         }
     }
 }
